fd8fccf2b3f3b1284eff7b63a0fe026e41abee49
Robotic-Brain <github@roboticbrain.de>
2015-08-07 09:19:00 +0200

    Initial commit
    Main functionality

---
+++ Classes/RoboticBrain/FlowContextHeader/Http/Component/FlowContextHeader.php
@@ -0,0 +1,42 @@
+<?php
+namespace RoboticBrain\FlowContextHeader\Http\Component;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "RoboticBrain.FlowContextHeader".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Http\Component\ComponentInterface;
+use TYPO3\Flow\Http\Component\ComponentContext;
+use TYPO3\Flow\Annotations as Flow;
+use RoboticBrain\FlowContextHeader\BootstrapProxy;
+
+class FlowContextHeader implements ComponentInterface {
+	
+	/**
+	 * @Flow\Inject
+	 * @var BootstrapProxy
+	 */
+	protected $bootstrapProxy;
+	
+	/**
+	 * @var array
+	 */
+	protected $options;
+	
+	/**
+	 * @param array $options
+	 */
+	public function __construct(array $options = array()) {
+		$this->options = $options;
+	}
+	
+	/**
+	 * @param ComponentContext $componentContext
+	 * @return void
+	 */
+	public function handle(ComponentContext $componentContext) {
+		$httpResponse = $componentContext->getHttpResponse();
+		$httpResponse->setHeader($this->options['headerName'], $this->bootstrapProxy->getBootstrapInstance()->getContext()->__toString());
+	}
+}
